9e616c45beeff404d2170a5fe5c443a7ee850c3b
Jacopo Antonello <jacopo@antonello.org>
2020-09-02 12:40:57 +0100

    Fix test for imports

--- test/test_imports.py
+++ test/test_imports.py
@@ -3,30 +3,27 @@
 
 import re
 import sys
-
 from subprocess import run
 from tempfile import TemporaryDirectory
 
-
 from devwraps.version import get_packages
-
 
 print()
 print('Test importing device libraries')
 devices = get_packages()
 with TemporaryDirectory() as tmpd:
     for d in devices:
-        p = run(
-            f'{sys.executable} -c "from devwraps import {d}; print({d})',
-            cwd=tmpd, capture_output=True, shell=True)
+        p = run(f'{sys.executable} -c "from devwraps import {d}; print({d})',
+                cwd=tmpd,
+                capture_output=True,
+                shell=True)
         out = p.stdout.decode().strip()
         err = re.search(r'(\w*Error)', p.stderr.decode().strip())
         try:
             err = err.group(1)
         except Exception:
             err = ''
-        out = re.sub(r"'.*", '', re.sub(r'.*\\', '', out))
-        if out.endswith('.pyd'):
+        if f'devwraps.{d}' in out:
             print(f'{d} success: {out}')
         else:
             print(f'{d} failure: {err}')
