19f400a43d75db61e6fa079fe54b5f6f80f43786
Jacopo Antonello <jacopo@antonello.org>
2021-02-23 18:35:04 +0000

    Fix bug in preset

--- devwraps/mirao52e.pyx
+++ devwraps/mirao52e.pyx
@@ -391,7 +391,9 @@ cdef class Mirao52e:
         else:
             raise NotImplementedError(name)
         u = np.zeros((MRO_NB_COMMAND_VALUES,))
-        u[np.unique(inds).sort() - 1] = mag
+        inds = np.unique(inds)
+        inds.sort()
+        u[inds] = mag
         return u
 
     def get_transform(self):
